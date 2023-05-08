05ac436312287106a322f6cfb6383b2a2757ea09
EldritchBunny <U2430273@rts-365.dk>
2023-05-08 09:18:32 +0200

    changed to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,25 @@
 import "./App.css";
-import MovieCard from "./templates/MovieCard";
-import Button from "./components/Button";
-import Heading from "./components/Heading";
-import Switch from "./components/Switch";
-import MovieItem from "./templates/MovieItem";
-import Navigation from "./components/Navigation";
 import DetailsView from "./pages/DetailsView";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import ListView from "./pages/ListView";
+import ErrorView from "./pages/ErrorView";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" errorElement={<ErrorView />}>
+      <Route path="" element={<ListView />} />
+      <Route path="details/:id" element={<DetailsView />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <DetailsView />
-    // <div className="App">
-    //   <header className="gridContainer header">
-    //     <Switch justify="end" align="center" />
-    //     <Heading title="MyMovies" size="16" as="h1" />
-    //   </header>
-    //   <main>
-    //     <section className="HorizontalMovieListContainer">
-    //       <div className="flexContainer justify-space-between">
-    //         <Heading title="Now Showing" size="16" as="h2" />
-    //         <Button title="See More" />
-    //       </div>
-    //       <div className="flexContainer movieCardContainerLayout">
-    //         <MovieCard />
-    //         <MovieCard />
-    //         <MovieCard />
-    //         <MovieCard />
-    //       </div>
-    //     </section>
-    //     <section className="VerticalMovieListContainer">
-    //       <div className="flexContainer justify-space-between VerticalMovieListTitleLayout">
-    //         <Heading title="Popular" size="16" as="h2" />
-    //         <Button title="See More" />
-    //       </div>
-    //       <div className="flexContainer movieListContainerLayout">
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //       </div>
-    //     </section>
-    //   </main>
-    //   <footer>
-    //     <Navigation />
-    //   </footer>
-    // </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
