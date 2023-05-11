801b8de17c854920e0edfca00ad5f69f73cccef8
EldritchBunny <U2430273@rts-365.dk>
2023-05-11 08:26:28 +0200

    .voteAverage was added to multiple files

--- src/templates/MovieItem.jsx
+++ src/templates/MovieItem.jsx
@@ -5,6 +5,7 @@ import Image from "../components/Image";
 import Label from "../components/Label";
 import Time from "../components/Time";
 import coverimage from "../assets/moviecover.png";
+import { Link, useLoaderData } from "react-router-dom";
 
 const StyledArticle = styled.article`
   display: flex;
@@ -20,20 +21,31 @@ const StyledDiv = styled.div`
   gap: 8px;
 `;
 const MovieItem = () => {
+  const movieCardData = useLoaderData();
   return (
-    <StyledArticle>
-      <Image src={coverimage} width="85" height="120" />
-      <StyledSection>
-        <Heading title="Venom Let There Be Carnage" size="14" as="h3" />
-        <Rating />
-        <StyledDiv>
-          <Label title="horror" />
-          <Label title="thriller" />
-          <Label title="documentary" />
-        </StyledDiv>
-        <Time />
-      </StyledSection>
-    </StyledArticle>
+    <>
+      {movieCardData.results.map((data) => (
+        <Link to="details/3" key={data.id}>
+          <StyledArticle>
+            <Image
+              src={`https://image.tmdb.org/t/p/w200/${data.poster_path}`}
+              width="85"
+              height="120"
+            />
+            <StyledSection>
+              <Heading title={data.title} size="14" as="h3" />
+              <Rating voteAverage={data.vote_average} />
+              <StyledDiv>
+                <Label title="horror" />
+                <Label title="thriller" />
+                <Label title="documentary" />
+              </StyledDiv>
+              <Time />
+            </StyledSection>
+          </StyledArticle>
+        </Link>
+      ))}
+    </>
   );
 };
 
